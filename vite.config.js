5c12d6cbad259537f8911ad48d83b6cd28a2ba67
manolors <manolo.rs2005@gmail.com>
2023-06-06 20:37:24 +0100

    Add env console.log

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
+console.log(process.env.NODE_ENV);
+
 export default {
   root: "src",
   base,
