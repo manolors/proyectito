7439148dc8be0978d22ee6d917d2242512fabe8c
manolors <manolo.rs2005@gmail.com>
2023-06-06 20:39:21 +0100

    remove console.log

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import path from "path";
-import console from "console";
 const isGitHubPages = false;
 const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
-
-console.log(process.env.NODE_ENV);
 
 export default {
   root: "src",
